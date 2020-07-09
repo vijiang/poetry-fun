d4005a25b9811c857415d334c4cbf64850fda190
vivian <vivianliujiang@gmail.com>
2020-07-09 17:03:57 -0400

    added code to tokeize poem content

--- poetry-thing.py
+++ poetry-thing.py
@@ -1,14 +1,41 @@
-import json
-import requests
-import csv
+import requests, json, csv
 from bs4 import BeautifulSoup
 from advanced_expiry_caching import Cache
 
 ## What do I want this project to do:
 
+# ------ code for caching ------
+FILENAME = "glass_essay_text.json"
+PROGRAM_CACHE = Cache(FILENAME)
 
 url = "https://www.poetryfoundation.org/poems/48636/the-glass-essay"
-poem_page = requests.get(url)
+# this url holds anne carson's poem
 
-print(type(poem_page))
+def access_page_data(url):
+    page_data = PROGRAM_CACHE.get(url)
+    if not page_data:
+        # if page_data does not exist, request to grab its content in text form
+        page_data = requests.get(url).text
+        PROGRAM_CACHE.set(url, page_data, expire_in_days=60)
+    return page_data
+
+poem_page = access_page_data(url)
+soup = BeautifulSoup(poem_page, "html.parser")
+poem_text = soup.find('div', {'class': 'o-poem'})
+
+all_poem_text = ""
+for line in poem_text.strings:
+    # print(line)
+    all_poem_text+=line
+
+tokenized_poem = all_poem_text.split()
+print(tokenized_poem)
+
+count = 0
+for item in tokenized_poem:
+    if item == " ":
+        count+=1
+print(count)
+
+
 
