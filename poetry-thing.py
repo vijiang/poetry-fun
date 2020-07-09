c96fe12d258ca98038d7c06546cdfbbdb186e9d8
vivian <vivianliujiang@gmail.com>
2020-07-09 15:55:20 -0400

    second commit, nothing important yet

--- poetry-thing.py
+++ poetry-thing.py
@@ -1,3 +1,14 @@
 import json
 import requests
+import csv
 from bs4 import BeautifulSoup
+from advanced_expiry_caching import Cache
+
+## What do I want this project to do:
+
+
+url = "https://www.poetryfoundation.org/poems/48636/the-glass-essay"
+poem_page = requests.get(url)
+
+print(type(poem_page))
+
